7502c84872eaa15e6f07a6956fe09d6c06ada42c
vakhrushev <vakhrushev@me.com>
2018-06-12 13:40:26 +0200

    something

---
+++ test.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-\
+#if __name__ == "__main__"
+#print('hello World')
+def print_palmtrees(sum):
+	for i in range(num):
+		sys.stdout.write('/|\\')
+	sys.stdout.write('\n')
+	for i in range(num):
+		sys.stdout.write('   |  ')
+	sys.stdout.write('\n\n')
